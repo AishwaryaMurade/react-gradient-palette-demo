bed2063effbd181e88d6dbed9b2ce278216a2990
Aishwarya Murade <87976092+AishwaryaMurade@users.noreply.github.com>
2024-02-05 19:00:56 +0530

    demo update

---
+++ precache-manifest.55f7310461c02896cf951b9ed7e61436.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479f96e9bdc80d530e07b5d43ef29407",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a48d992804b6eab8156c",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "49c47456205b1cfd4f90",
+    "url": "./static/js/2.f10a24fd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f10a24fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a48d992804b6eab8156c",
+    "url": "./static/js/main.5efac1dc.chunk.js"
+  },
+  {
+    "revision": "afda40779c0bfb35b78a",
+    "url": "./static/js/runtime-main.1ef47b6a.js"
+  }
+]);
